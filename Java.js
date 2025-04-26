7c88e009443413138833c8eec2b0384975034136
ndewtyme77 <dewilliams01@gmail.com>
2025-04-25 23:48:14 -0400

    contact form

--- Java.js
+++ Java.js
@@ -1,6 +1,6 @@
 const scriptURL = 'https://script.google.com/u/0/home/projects/1zLh0551FtGLPKMdDCOWqzfDlVcEKJjdEN5IRTUaLT2G2Q57drXtxz8N5/exec'
 
-const form = document.forms['contact']
+const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
 
