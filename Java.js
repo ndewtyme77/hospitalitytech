01261f192f907211d83b9e71192740552b892f75
ndewtyme77 <dewilliams01@gmail.com>
2025-04-25 23:58:47 -0400

    Test

--- Java.js
+++ Java.js
@@ -1,13 +1,13 @@
-const scriptURL = 'https://script.google.com/u/0/home/projects/1zLh0551FtGLPKMdDCOWqzfDlVcEKJjdEN5IRTUaLT2G2Q57drXtxz8N5/exec'
+const scriptURL = 'https://script.google.com/u/0/home/projects/1zLh0551FtGLPKMdDCOWqzfDlVcEKJjdEN5IRTUaLT2G2Q57drXtxz8N5/exec';
 
-const form = document.forms['contact-form']
+const form = document.forms['contact-form'];
 
 form.addEventListener('submit', e => {
 
-  e.preventDefault()
+  e.preventDefault();
 
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
   .then(response => alert("Thank you! Form is submitted" ))
   .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+  .catch(error => console.error('Error!', error.message));
+});
