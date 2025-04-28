729f6f6af2df0cfe015d8be62f07d175868e01e8
ndewtyme77 <dewilliams01@gmail.com>
2025-04-28 17:48:41 -0400

    Navibar

---
+++ scripts/Navibar.js
@@ -0,0 +1,33 @@
+// add classes for mobile navigation toggling
+var body = document.querySelector('body');
+const navbarMenu = document.querySelector('#navigation');
+const hamburgerMenu = document.querySelector('#navigation .toggle');
+
+CShamburgerMenu.addEventListener('click', function () {
+	hamburgerMenu.classList.toggle('active');
+	navbarMenu.classList.toggle('active');
+	body.classList.toggle('open');
+	// run the function to check the aria-expanded value
+	ariaExpanded();
+});
+
+// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
+function ariaExpanded() {
+	const UL = document.querySelector('#expanded');
+	const Expanded = csUL.getAttribute('aria-expanded');
+
+	if (Expanded === 'false') {
+		UL.setAttribute('aria-expanded', 'true');
+	} else {
+		UL.setAttribute('aria-expanded', 'false');
+	}
+}
+
+// mobile nav toggle code
+const dropDowns = Array.from(document.querySelectorAll('#navigation .dropdown'));
+for (const item of dropDowns) {
+	const onClick = () => {
+		item.classList.toggle('active');
+	};
+	item.addEventListener('click', onClick);
+}
